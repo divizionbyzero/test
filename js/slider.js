067b56b662765ebbaff3859dd2a73d0dfc56cbd0
divizionbyzero <divizionbyzero@bk.ru>
2015-06-08 12:46:42 +0300

    some fixes

--- js/slider.js
+++ js/slider.js
@@ -46,43 +46,46 @@ jQuery(document).ready(function () {
 
     }
 
+    var productHalfBlock = '.twice:nth-last-of-type(n+2)';
+    var productBlock = '.fourfold:nth-last-of-type(n+2)';
+    
     htmSlider();
-
     $('.content__section-view-all').click(function (e) {
         $(e.target).addClass('clicked');
         if ($('body').width() < 582) {
-            $(e.target).closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').addClass('block-display');
+            $('.clicked').closest('.content__section').find(productBlock).removeClass('flex-display');
+            $('.clicked').closest('.content__section').find(productHalfBlock).removeClass('flex-display');
+            $(e.target).closest('.content__section').find(productBlock).addClass('block-display');
         }
         else if ($('body').width() > 781) {
-            $(e.target).closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').addClass('flex-display');
-            $(e.target).closest('.content__section').find('.twice:nth-last-of-type(n+2)').addClass('flex-display');
+            $(e.target).closest('.content__section').find(productBlock).addClass('flex-display');
+            $(e.target).closest('.content__section').find(productHalfBlock).addClass('flex-display');
         }
         else {
-            $(e.target).closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').addClass('block-display');
-            $(e.target).closest('.content__section').find('.twice:nth-last-of-type(n+2)').addClass('flex-display');
+            $(e.target).closest('.content__section').find(productBlock).addClass('block-display');
+            $(e.target).closest('.content__section').find(productHalfBlock).addClass('flex-display');
         }
     });
 
     $(window).resize(function () {
-
-        if (($('body').width() < 582) && ( $('.clicked').closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').hasClass('block-display') == true)) {
-            $('.clicked').closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').removeClass('flex-display');
-            $('.clicked').closest('.content__section').find('.twice:nth-last-of-type(n+2)').removeClass('flex-display');
-            $('.clicked').closest('.content__section').find('.twice:nth-last-of-type(n+2)').removeClass('block-display');
-            $('.clicked').closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').addClass('block-display');
+        if (($('body').width() < 582) && ( $('.clicked').closest('.content__section').find(productBlock).hasClass('block-display') == true)) {
+            $('.clicked').closest('.content__section').find(productBlock).removeClass('flex-display');
+            $('.clicked').closest('.content__section').find(productHalfBlock).removeClass('flex-display');
+            $('.clicked').closest('.content__section').find(productHalfBlock).removeClass('block-display');
+            $('.clicked').closest('.content__section').find(productBlock).addClass('block-display');
 
         }
-        if (($('body').width() > 781) && (($('.clicked').closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').hasClass('block-display') == true) || ($('.clicked').closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').hasClass('flex-display') == true))) {
-            $('.clicked').closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').removeClass('block-display');
-            $('.clicked').closest('.content__section').find('.twice:nth-last-of-type(n+2)').removeClass('block-display');
-            $('.clicked').closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').addClass('flex-display');
-            $('.clicked').closest('.content__section').find('.twice:nth-last-of-type(n+2)').addClass('flex-display');
+        if (($('body').width() > 781) && (($('.clicked').closest('.content__section').find(productBlock).hasClass('block-display') == true) || ($('.clicked').closest('.content__section').find(productBlock).hasClass('flex-display') == true))) {
+            $('.clicked').closest('.content__section').find(productBlock).removeClass('block-display');
+            $('.clicked').closest('.content__section').find(productHalfBlock).removeClass('block-display');
+            $('.clicked').closest('.content__section').find(productBlock).addClass('flex-display');
+            $('.clicked').closest('.content__section').find(productHalfBlock).addClass('flex-display');
         }
-        if ((($('body').width() > 581) && ($('body').width() < 782)) && (($('.clicked').closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').hasClass('block-display') == true) || ($('.clicked').closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').hasClass('flex-display') == true))) {
-            $('.clicked').closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').removeClass('flex-display');
-            $('.clicked').closest('.content__section').find('.twice:nth-last-of-type(n+2)').removeClass('block-display');
-            $('.clicked').closest('.content__section').find('.fourfold:nth-last-of-type(n+2)').addClass('block-display');
-            $('.clicked').closest('.content__section').find('.twice:nth-last-of-type(n+2)').addClass('flex-display');
+        if ((($('body').width() > 581) && ($('body').width() < 782)) && (($('.clicked').closest('.content__section').find(productBlock).hasClass('block-display') == true) || ($('.clicked').closest('.content__section').find(productBlock).hasClass('flex-display') == true))) {
+            $('.clicked').closest('.content__section').find(productBlock).removeClass('flex-display');
+            $('.clicked').closest('.content__section').find(productHalfBlock).removeClass('block-display');
+            $('.clicked').closest('.content__section').find(productBlock).addClass('block-display');
+            $('.clicked').closest('.content__section').find(productHalfBlock).addClass('flex-display');
         }
 
     });
